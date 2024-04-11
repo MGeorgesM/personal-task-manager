cd09a0e5f663a6b5e7728e7ea3ae76e6440c752c
George <g.m.6@live.com>
2024-04-11 18:39:23 +0300

    controllers

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,13 +7,13 @@ const register = async (req, res) => {
 
     try {
         const existingUser = await User.findOne({ email });
-        existingUser && res.status(400).send('User already exists');
+        existingUser && res.status(400).send({ error: 'User already exists' });
 
         const createdUser = await User.create({ fullName, email, password });
         return res.status(201).json(createdUser);
     } catch (error) {
         console.log(error);
-        return res.status(500).json('Internal server error');
+        return res.status(500).json({error: 'Error while registering'});
     }
 };
 
@@ -25,13 +25,13 @@ const login = async (req, res) => {
         !user && res.status(400).json('User not found');
 
         const isPasswordMatch = user.comparePassword(plainTextPassword);
-        !isPasswordMatch && res.status(400).json('Invalid credentials');
+        !isPasswordMatch && res.status(400).json({ error: 'Invalid credentials' });
 
         const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
         return res.status(200).json({ token });
     } catch (error) {
         console.log(error);
-        return res.status(500).json('Internal server error');
+        return res.status(500).json({error: 'Error while signing in'});
     }
 };
 

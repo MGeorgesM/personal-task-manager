b0d380574aedfe1f4df0937e4871aa5fd293ef49
George <g.m.6@live.com>
2024-04-11 18:04:02 +0300

    models

--- api/models/tag.model.js
+++ api/models/tag.model.js
@@ -11,6 +11,11 @@ const tagSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
     },
 );
 

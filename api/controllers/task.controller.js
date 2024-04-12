ba3324ce48e48b14e8f9b12bcf39abfef1500cec
George <g.m.6@live.com>
2024-04-13 01:49:34 +0300

    refactoring the api

--- api/controllers/task.controller.js
+++ api/controllers/task.controller.js
@@ -1,19 +1,23 @@
-const { Task } = require('../models/task.model');
+const { User } = require('../models/user.model');
 
 const createTask = async (req, res) => {
-    const { title, description, attachment, tags, column } = req.body;
+    const { title, description, attachment, tags, columnId } = req.body;
     const userId = req.user._id;
 
     try {
-        const createdTask = await Task.create({
-            title,
-            description,
-            attachment,
-            tags,
-            column,
-            owner: userId,
-        });
-        return res.status(201).json(createdTask);
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        const board = user.boards.find((board) => board.columns.find((column) => column._id.toString() === columnId));
+        if (!board) return res.status(404).json({ error: 'Board not found' });
+
+        const column = board.columns.find((column) => column._id.toString() === columnId);
+        if (!column) return res.status(404).json({ error: 'Column not found' });
+
+        column.tasks.push({ title, description, attachment, tags });
+        await user.save();
+
+        return res.status(201).json(column.tasks[column.tasks.length - 1]);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: 'Error while creating task' });
@@ -25,13 +29,16 @@ const getTasks = async (req, res) => {
     const userId = req.user._id;
 
     try {
-        if (columnId) {
-            const tasks = await Task.find({ column: columnId });
-            return res.status(200).json(tasks);
-        } else {
-            const tasks = await Task.find({ owner: userId });
-            return res.status(200).json(tasks);
-        }
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        const board = user.boards.find((board) => board.columns.find((column) => column._id.toString() === columnId));
+        if (!board) return res.status(404).json({ error: 'Board not found' });
+
+        const column = board.columns.find((column) => column._id.toString() === columnId);
+        if (!column) return res.status(404).json({ error: 'Column not found' });
+
+        return res.status(200).json(column.tasks);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: 'Error while fetching tasks' });
@@ -40,29 +47,87 @@ const getTasks = async (req, res) => {
 
 const updateTask = async (req, res) => {
     const id = req.params.id;
+    const userId = req.user._id;
     const { title, description, attachment, tags, columnId } = req.body;
 
     try {
-        const task = await Task.findByIdAndUpdate(
-            id,
-            { title, description, attachment, tags, column: columnId },
-            { new: true }
-        );
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ error: 'User not found' });
 
+        let task;
+
+        if (columnId) {
+            let currentColumn;
+            let destinationColumn;
+
+            user.boards.forEach(board => {
+                board.columns.forEach(column => {
+                    if (column.tasks.find(task => task._id.toString() === id)) {
+                        currentColumn = column;
+                    }
+                    if (column._id.toString() === columnId) {
+                        destinationColumn = column;
+                    }
+                });
+            });
+
+            if (!currentColumn || !destinationColumn) return res.status(404).json({ error: 'Column not found' });
+
+            if (currentColumn._id.toString() !== destinationColumn._id.toString()) {
+                const taskIndex = currentColumn.tasks.findIndex(task => task._id.toString() === id);
+                if (taskIndex === -1) return res.status(404).json({ error: 'Task not found' });
+
+                [task] = currentColumn.tasks.splice(taskIndex, 1);
+
+                destinationColumn.tasks.push(task);
+            }
+        }
+
+        if (!task) {
+            user.boards.forEach(board => {
+                board.columns.forEach(column => {
+                    const foundTask = column.tasks.find(task => task._id.toString() === id);
+                    if (foundTask) task = foundTask;
+                });
+            });
+        }
+
+        if (!task) return res.status(404).json({ error: 'Task not found' });
+
+        title && (task.title = title);
+        description && (task.description = description);
+        attachment && (task.attachment = attachment);
+        tags && (task.tags = tags);
+    
+        await user.save();
         return res.status(200).json(task);
     } catch (error) {
-        console.log(error);
+        console.error(error);
         return res.status(500).json({ error: 'Error while updating task' });
     }
 };
 
 const deleteTask = async (req, res) => {
-    const id = req.params.id;
+    const { boardId, columnId, taskId } = req.params;
     const userId = req.user._id;
 
     try {
-        const deletedTask = await Task.findOneAndDelete({ _id: id, owner: userId });
-        return res.status(200).json(deletedTask);
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        const board = user.boards.find(board => board._id.toString() === boardId);
+        if (!board) return res.status(404).json({ error: 'Board not found' });
+
+        const column = board.columns.find(column => column._id.toString() === columnId);
+        if (!column) return res.status(404).json({ error: 'Column not found' });
+
+        const taskIndex = column.tasks.findIndex(task => task._id.toString() === taskId);
+        if (taskIndex === -1) return res.status(404).json({ error: 'Task not found' });
+
+        column.tasks.splice(taskIndex, 1);
+        await user.save();
+
+        return res.status(200).json({ message: 'Task deleted successfully' });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: 'Error while deleting task' });

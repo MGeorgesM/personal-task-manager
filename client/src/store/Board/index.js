1cd5ceb7e140b125cabc0be51f0ff9c25d978c30
George <g.m.6@live.com>
2024-04-13 12:08:18 +0300

    api tests done -tasks setting up redux

---
+++ client/src/store/Board/index.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    boards: [],
+    loading: false,
+    error: null,
+};
+
+const boardSlice = createSlice({
+    name: 'boardSlice',
+    initialState,
+    reducers: {
+        getBoards: (state, action) => {
+            state.boards = action.payload;
+        },
+        addBoard: (state, action) => {
+            state.boards.push(action.payload);
+        },
+        updateBoard: (state, action) => {
+            const { _id, title, description } = action.payload;
+            const boardIndex = state.boards.findIndex((board) => board._id === _id);
+            if (boardIndex !== -1) {
+                state.boards[boardIndex].title = title || state.boards[boardIndex].title;
+                state.boards[boardIndex].description = description || state.boards[boardIndex].description;
+            }
+        },
+        deleteBoard: (state, action) => {
+            const boardId = action.payload;
+            state.boards = state.boards.filter((board) => board._id !== boardId);
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
+    },
+});
+
+export const { getBoards, addBoard, updateBoard, deleteBoard, setLoading, setError } = boardSlice.actions;
+export const boardSliceName = boardSlice.name;
+export default boardSlice.reducer;
---
+++ client/src/store/User/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    currentUser: null,
+    isLoggedIn: false,
+};
+
+export const userSlice = createSlice({
+    name: 'userSlice',
+    initialState,
+    reducers: {
+        setCurrentUser: (state, action) => {
+            state.currentUser = action.payload;
+            state.isLoggedIn = true;
+        },
+        clearCurrentUser: (state) => {
+            state.currentUser = null;
+            state.isLoggedIn = false;
+        },
+    },
+});
+
+export const { setCurrentUser, clearCurrentUser } = userSlice.actions;
+export const userSliceName = userSlice.name;
+export default userSlice.reducer;
